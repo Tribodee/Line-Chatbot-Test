d843a5bf07319468345a9936f378a4ac5c9a4637
Tribodee <riggker@gmail.com>
2023-11-07 19:34:13 +0700

    new api

--- index.js
+++ index.js
@@ -22,27 +22,43 @@ app.post('/webhook', line.middleware(lineConfig), async (req, res) =>{
     }
 });
 const handleEvent = async (event) => {
-    if (event.type === 'message' && event.message.text == 'test'){
-        const UserText = event.message.text
+    if (event.type === 'message' && event.message.text == 'Game Free'){
         try {
-            const {data} = await axios.get(`https://${env.RAPID_URL}`,{
+            const {data} = await axios.get(`https://${env.RAPID_URL}/epic-free-games`,{
                 headers: {
                     'X-RapidAPI-Host': env.RAPID_URL,
                     'X-RapidAPI-Key': env.RAPID_KEY
                 }
             })
             console.log("Data=>>>>",data)
-            return client.replyMessage(event.replyToken, { type: 'text', text: 'test' });
+            const dataformatted = data.map(item => ({
+                name: item.name,
+                description: item.description,
+                originalPrice: item.originalPrice,
+                appUrl: item.appUrl,
+                offerImageOrigin: item.offerImageTall,
+                offerImagePreview: item.offerImageWide
+            }))
+            const message = dataformatted.map(data => `Game Name: \n${data.name}\nDescription: \n${data.description}\nFull Price: ${data.originalPrice}\nlink: \n${data.appUrl}`).join('\n\n');
+            const lineimgOrigin = dataformatted.map(data => `${data.offerImageOrigin}`).join('\n\n');
+            const lineimgPreview = dataformatted.map(data => `${data.offerImagePreview}`).join('\n\n');
+            console.log("Data_message=>>>>",message)
+            console.log("Img=>>>>",lineimgOrigin,lineimgPreview)
+            return client.replyMessage(event.replyToken, [
+                { type: 'text', text: message },
+                { type: 'image', originalContentUrl: lineimgOrigin, previewImageUrl: lineimgPreview }
+            ]);
         } catch (error) {
             console.error('Something wrong on API:', error);
             return client.replyMessage(event.replyToken, { type: 'text', text: 'Something wrong on API' });
         }
     }
     else if(event.type !== 'message' || event.message.type !== 'text'){
-        return client.replyMessage(event.replyToken,{type:'text',text:'where\'s test'})
+        return client.replyMessage(event.replyToken,{type:'text',text:'error'})
     }
 }
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
+const PORT = 4000 //for test
 app.listen(PORT, () => {
     console.log(PORT)
     console.log(`Listening on port ${PORT}`);
